47ed03d5d81a6cd208d29afbec4c6bf4f5a50e0f
Victoria Santos <victoria.emilly.12@gmail.com>
2022-08-14 19:06:39 -0300

    update e configuração de responsividade

--- script.js
+++ script.js
@@ -15,3 +15,8 @@ sr.reveal('.sessao-b-texto,.titulo3',slide,{delay: 300, origin:'right'});
 sr.reveal('#victoria,.sessao-c-habilidades,.conteudo-projeto,#rodape',slide,{delay: 500, origin:'bottom', interval:200});
 sr.reveal('.sessao-a-texto',slideHome,{delay:200, origin:'left'});
 sr.reveal('.aside,.btn-contatos',slideHome,{delay:400, origin:'left',interval:200});
+
+/* função para fechar o menu ao clicar em um dos botões*/
+function closeNav(){
+    document.getElementById('menu-toggle').checked=false;
+}
